311c0bd28a0db07283b4e7f32cd6e80f06003317
Shaik_Z <kz981786@gmail.com>
2020-11-15 22:13:13 +0530

    jhhkn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
 app.use(express.urlencoded());
-const Joi= require("joi");
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
@@ -17,31 +16,21 @@ app.get('/',(req,res)=>{
     res.send("Hello world!");
 })
 
-const schema= Joi.object({
-    num1: Joi.number().min(-1000000).max(1000000),
-    num2: Joi.number().min(-1000000).max(1000000)
-});
-
 app.post('/add',(req,res)=>{
-    const validateNumbers= schema.validate(req.body);
-    // console.log(validateNumbers.error.details[0].message);
     const num1= req.body.num1
     const num2= req.body.num2
 
-    if(validateNumbers.error){
-        const errmessage= validateNumbers.error.details[0].message;
-        if(errmessage.includes("must be a number")) {
-            res.send({status: "error", message: "Invalid data types", sum: 0});
-            return;
-        }
-        else if(errmessage.includes("must be less than or equal to 1000000")){
-            res.send({status:"error",message:"Overflow",sum:0});
-            return ;
-        }
-        else if(errmessage.includes("must be greater than or equal to -1000000")){
-            res.send({status:"error",message:"Underflow",sum:0});
-            return ;
-        }
+    if(typeof num1!=='number' || typeof num2!=='number') {
+        res.send({status: "error", message: "Invalid data types", sum: 0});
+        return;
+    }
+    else if(num1>1000000 || num2>1000000 || num1+num2>1000000){
+        res.send({status:"error",message:"Overflow",sum:0});
+        return ;
+    }
+    else if(num1<-1000000 || num2<-1000000 || num1+num2<-1000000){
+        res.send({status:"error",message:"Underflow",sum:0});
+        return ;
     }
     res.send({
         status: "success",
@@ -51,25 +40,20 @@ app.post('/add',(req,res)=>{
 })
 
 app.post('/sub',(req,res)=>{
-    const validateNumbers= schema.validate(req.body);
-    // console.log(validateNumbers.error.details[0].message);
     const num1= req.body.num1
     const num2= req.body.num2
 
-    if(validateNumbers.error){
-        const errmessage= validateNumbers.error.details[0].message;
-        if(errmessage.includes("must be a number")) {
-            res.send({status: "error", message: "Invalid data types", difference: 0});
-            return;
-        }
-        else if(errmessage.includes("must be less than or equal to 1000000")){
-            res.send({status:"error",message:"Overflow",difference:0});
-            return ;
-        }
-        else if(errmessage.includes("must be greater than or equal to -1000000")){
-            res.send({status:"error",message:"Underflow",difference:0});
-            return ;
-        }
+    if(typeof num1!=='number' || typeof num2!=='number') {
+        res.send({status: "error", message: "Invalid data types", difference: 0});
+        return;
+    }
+    else if(num1>1000000 || num2>1000000){
+        res.send({status:"error",message:"Overflow",difference:0});
+        return ;
+    }
+    else if(num1<-1000000 || num2<-1000000){
+        res.send({status:"error",message:"Underflow",difference:0});
+        return ;
     }
     res.send({
         status: "success",
@@ -79,25 +63,20 @@ app.post('/sub',(req,res)=>{
 })
 
 app.post('/multiply',(req,res)=>{
-    const validateNumbers= schema.validate(req.body);
-    // console.log(validateNumbers.error.details[0].message);
     const num1= req.body.num1
     const num2= req.body.num2
 
-    if(validateNumbers.error){
-        const errmessage= validateNumbers.error.details[0].message;
-        if(errmessage.includes("must be a number")) {
-            res.send({status: "error", message: "Invalid data types", result: 0});
-            return;
-        }
-        else if(errmessage.includes("must be less than or equal to 1000000")){
-            res.send({status:"error",message:"Overflow",result:0});
-            return ;
-        }
-        else if(errmessage.includes("must be greater than or equal to -1000000")){
-            res.send({status:"error",message:"Underflow",result:0});
-            return ;
-        }
+    if(typeof num1!=='number' || typeof num2!=='number') {
+        res.send({status: "error", message: "Invalid data types", result: 0});
+        return;
+    }
+    else if(num1>1000000 || num2>1000000 || num1*num2>1000000){
+        res.send({status:"error",message:"Overflow",result:0});
+        return ;
+    }
+    else if(num1<-1000000 || num2<-1000000 || num1*num2<-1000000){
+        res.send({status:"error",message:"Underflow",result:0});
+        return ;
     }
     res.send({
         status: "success",
@@ -107,25 +86,20 @@ app.post('/multiply',(req,res)=>{
 })
 
 app.post('/divide',(req,res)=>{
-    const validateNumbers= schema.validate(req.body);
-    // console.log(validateNumbers.error.details[0].message);
     const num1= req.body.num1
     const num2= req.body.num2
 
-    if(validateNumbers.error){
-        const errmessage= validateNumbers.error.details[0].message;
-        if(errmessage.includes("must be a number")) {
-            res.send({status: "error", message: "Invalid data types", result: 0});
-            return;
-        }
-        else if(errmessage.includes("must be less than or equal to 1000000")){
-            res.send({status:"error",message:"Overflow",result:0});
-            return ;
-        }
-        else if(errmessage.includes("must be greater than or equal to -1000000")){
-            res.send({status:"error",message:"Underflow",result:0});
-            return ;
-        }
+    if(typeof num1!=='number' || typeof num2!=='number') {
+        res.send({status: "error", message: "Invalid data types", result: 0});
+        return;
+    }
+    else if(num1>1000000 || num2>1000000){
+        res.send({status:"error",message:"Overflow",result:0});
+        return ;
+    }
+    else if(num1<-1000000 || num2<-1000000){
+        res.send({status:"error",message:"Underflow",result:0});
+        return ;
     }
     if(num2===0){
         res.send({status:"error",message:"Cannot divide by zero",result:0});
